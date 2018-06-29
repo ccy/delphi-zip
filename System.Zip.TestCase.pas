9a4b2e10f386c311f087e4bdf52005d4775a13d8
Olivier DAHON <olivier.dahon@gmail.com>
2018-06-29 14:21:25 +0200

    Zip64 changes
    Add support for large number of files (more than 65535)
    Fix issues/warnings

--- System.Zip.TestCase.pas
+++ System.Zip.TestCase.pas
@@ -2,14 +2,24 @@ unit System.Zip.TestCase;
 
 interface
 
-uses TestFramework, System.Zip2;
+uses System.Classes, System.SysUtils, TestFramework, System.Zip2;
 
 type
   TTestCase_Zip = class(TTestCase)
+  private
+    class function ReadFile (const APath : string) : TBytes; static;
+    class procedure ReadBuffer (AStream       : TStream;
+                                var   Buffer  : TBytes;
+                                Offset, Count : Int64); static;
+    function CreateFile(const AExtension: string): string;
   protected
     function GetCompression: TZipCompression; virtual; abstract;
   published
     procedure TestCase_Zip_File;
+{$IFDEF WIN64}
+    procedure TestCase_BigZip_File;
+{$ENDIF}
+    procedure TestCase_MultipleFile;
   end;
 
   TTestCase_Zip_zcStored = class(TTestCase_Zip)
@@ -29,7 +39,176 @@ type
 
 implementation
 
-uses Classes, System.SysUtils, System.IOUtils, System.Zip.LZMA;
+uses System.IOUtils, System.RTLConsts, System.Types, System.Zip.LZMA;
+
+function TTestCase_Zip.CreateFile(const AExtension: string): string;
+  var
+    LCheminTemp : string;
+BEGIN
+  LCheminTemp := TPath.GetTempFileName;
+  try
+    Result := TPath.ChangeExtension(LCheminTemp, AExtension);
+  finally
+    TFile.Delete(LCheminTemp);
+  end;
+end;
+
+class procedure TTestCase_Zip.ReadBuffer(AStream       : TStream;
+                                         var   Buffer  : TBytes;
+                                         Offset, Count : Int64);
+  var
+    LTotalCount,
+    LReadCount : NativeInt;
+begin
+  { Perform a read directly. Most of the time this will succeed
+    without the need to go into the WHILE loop. }
+  LTotalCount := AStream.Read64(Buffer, Offset, Count);
+  { Check if there was an error }
+  if LTotalCount < 0 then
+    raise EReadError.CreateRes(@SReadError);
+
+  while (LTotalCount < Count) DO
+  begin
+    { Try to read a contiguous block of <Count> size }
+    LReadCount := AStream.Read64(Buffer, Offset + LTotalCount, (Count - LTotalCount));
+    { Check if we read something and decrease the number of bytes left to read }
+    if LReadCount <= 0 then
+      raise EReadError.CreateRes(@SReadError)
+    else
+      Inc(LTotalCount, LReadCount);
+  end;
+end;
+
+class function TTestCase_Zip.ReadFile(const APath: string): TBytes;
+  var
+    LFileStream : TFileStream;
+begin
+  LFileStream := NIL;
+  try
+    LFileStream := TFileStream.Create(APath, fmOpenRead or fmShareDenyWrite);
+    SetLength(Result, LFileStream.Size);
+
+    ReadBuffer(LFileStream, Result, 0, Length(Result));
+  finally
+    LFileStream.Free;
+  end;
+end;
+
+{$IFDEF WIN64}
+procedure TTestCase_Zip.TestCase_BigZip_File;
+  var
+    LCheminZip,
+    LCheminFichier : string;
+    LZipFile       : TZipFile;
+    LOutput        : TBytes;
+    LFileStream    : TFileStream;
+    LLongueurVoulue,
+    LLongueur      : Int64;
+BEGIN
+  LCheminZip := CreateFile ('.zip');
+  LCheminFichier := CreateFile ('.bin');
+
+  LFileStream := NIL;
+  try
+    LFileStream := TFile.Create (LCheminFichier);
+    LLongueurVoulue := $100000000;
+    LFileStream.Size := LLongueurVoulue;
+  finally
+    LFileStream.Free;
+  end;
+
+  // Compress File
+  LZipFile := TZipFile.Create;
+  LZipFile.Open(LCheminZip, zmWrite);
+  try
+    try
+      LZipFile.Add(LCheminFichier, '', GetCompression);
+    finally
+      LZipFile.Close;
+      LZipFile.Free;
+      TFile.Delete(LCheminFichier);
+    end;
+
+    // Decompress File
+    TZipFile.ExtractZipFile(LCheminZip, TPath.GetTempPath);
+  finally
+    TFile.Delete(LCheminZip);
+  end;
+
+  CheckTrue(TFile.Exists(LCheminFichier));
+
+  try
+    LOutput := ReadFile(LCheminFichier);
+    LLongueur := Length(LOutput);
+    CheckEquals(LLongueurVoulue, LLongueur);
+  finally
+    TFile.Delete(LCheminFichier);
+  end;
+end;
+{$ENDIF}
+
+procedure TTestCase_Zip.TestCase_MultipleFile;
+  const
+    C_NbFichier : Integer = 65536;
+  var
+    LCheminZip,
+    LCheminTemp,
+    LNomFichier,
+    LCheminFichier  : string;
+    LI              : Integer;
+    LZipFile        : TZipFile;
+    LInput, LOutput : TBytes;
+    LFiles          : TStringDynArray;
+BEGIN
+  LCheminZip := CreateFile ('.zip');
+  LCheminFichier := CreateFile ('.bin');
+
+  Randomize;
+  SetLength(LInput, 10);
+  for LI := Low(LInput) to High(LInput) do
+    LInput[LI] := Random(128);
+
+  TFile.WriteAllBytes(LCheminFichier, LInput);
+
+  // Compress File
+  LZipFile := TZipFile.Create;
+  LZipFile.Open(LCheminZip, zmWrite);
+  try
+    try
+      LNomFichier := TPath.GetFileName(LCheminFichier);
+      for LI := 1 to C_NbFichier do
+        LZipFile.Add (LCheminFichier,
+                      string.Format('%0:s.%1:d', [LNomFichier, LI]),
+                      GetCompression);
+    finally
+      LZipFile.Close;
+      LZipFile.Free;
+      TFile.Delete(LCheminFichier);
+    end;
+
+    // Decompress File
+    LCheminTemp := LCheminFichier.Substring(0, length (LCheminFichier) - 4);
+    TDirectory.CreateDirectory(LCheminTemp);
+
+    try
+      TZipFile.ExtractZipFile(LCheminZip, LCheminTemp);
+
+      LFiles := TDirectory.GetFiles(LCheminTemp);
+      CheckEquals(C_NbFichier, Length (LFiles));
+
+      for LI:= 0 to C_NbFichier - 1 do
+      begin
+        LOutput := TFile.ReadAllBytes(LFiles[LI]);
+        CheckEquals(Length(LInput), Length(LOutput));
+        CheckTrue(CompareMem(LInput, LOutput, Length(LInput)));
+      end;
+    finally
+      TDirectory.Delete(LCheminTemp, True);
+    end;
+  finally
+    TFile.Delete(LCheminZip);
+  end;
+end;
 
 procedure TTestCase_Zip.TestCase_Zip_File;
 var fZip, fData: string;
