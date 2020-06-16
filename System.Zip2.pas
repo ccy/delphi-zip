4aa6378c73e9e6bcebb9ff2ce0ec0b8e3f62ab58
Chau Chee Yang <ccy@sql.com.my>
2020-06-16 10:12:01 +0800

    Update to Embarcadero® Delphi 10.4 Version 27.0.37889.9797

--- System.Zip2.pas
+++ System.Zip2.pas
@@ -2,7 +2,7 @@
 {                                                       }
 {           CodeGear Delphi Runtime Library             }
 {                                                       }
-{ Copyright(c) 1995-2019 Embarcadero Technologies, Inc. }
+{ Copyright(c) 1995-2020 Embarcadero Technologies, Inc. }
 {              All rights reserved                      }
 {                                                       }
 {   Copyright and license exceptions noted in source    }
