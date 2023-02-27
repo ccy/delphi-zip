332d2325323fca5fe229cc3b4fe91ce56b711706
Chau Chee Yang <ccy@sql.com.my>
2023-02-27 18:11:43 +0800

    Revise project files

--- ZipTest.dpr
+++ ZipTest.dpr
@@ -4,6 +4,12 @@ program ZipTest;
 
 uses
   TextTestRunner,
+  Threads in 'lzmasdk\Delphi\Threads.pas',
+  LzmaTypes in 'lzmasdk\Delphi\LzmaTypes.pas',
+  LzFind in 'lzmasdk\Delphi\LzFind.pas',
+  LzFindMt in 'lzmasdk\Delphi\LzFindMt.pas',
+  LzmaDec in 'lzmasdk\Delphi\LzmaDec.pas',
+  LzmaEnc in 'lzmasdk\Delphi\LzmaEnc.pas',
   System.Zip2 in 'System.Zip2.pas',
   System.Zip.LZMA in 'System.Zip.LZMA.pas',
   System.Zip.TestCase in 'System.Zip.TestCase.pas';
